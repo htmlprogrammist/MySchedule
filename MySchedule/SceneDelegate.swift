7c42e1599ee37bf8a9980e6cd94465cb523ebac6
Egor Badmaev <badmaeve2511@gmail.com>
2021-12-10 18:32:09 +0300

    Revert "refactor: add groups for files"
    This reverts commit 0beabfd8fe2944f962cc24f50f0c59c193368d6e.

--- MySchedule/SceneDelegate.swift
+++ MySchedule/SceneDelegate.swift
@@ -19,8 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = ViewController()
         window.makeKeyAndVisible()
-        window.overrideUserInterfaceStyle = .light
-        // self.window = window
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
