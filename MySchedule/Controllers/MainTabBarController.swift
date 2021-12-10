04bd9400bb6a8c66cbbe330ac9ed8022fd39d93e
Egor Badmaev <badmaeve2511@gmail.com>
2021-12-10 20:09:42 +0300

    refactor: добавил комментариев, просто информация

--- MySchedule/Controllers/MainTabBarController.swift
+++ MySchedule/Controllers/MainTabBarController.swift
@@ -25,10 +25,15 @@ class MainTabBarController: UITabBarController {
     
     func createNavController(viewController: UIViewController, itemName: String, itemImage: String) -> UINavigationController {
         // systemName появился только с iOS 14, вроде как можно будет просто named указать
-        let item = UITabBarItem(title: itemName, image: UIImage(systemName: itemImage)?.withAlignmentRectInsets(.init(top: 10, left: 0, bottom: 0, right: 0)), tag: 0)
+        let item = UITabBarItem(title: itemName, image: UIImage(systemName: itemImage), tag: 0)
+        // let item = UITabBarItem(title: itemName, image: UIImage(named: itemImage)?.withRenderingMode(.alwaysOriginal), tag: 0)  // что-то не работает
+        
+        // по мнению автора, нужно опустить табБарАйтемы, потому что они слишком высоко, но мне так не укажется, поэтому вот.
+        // let item = UITabBarItem(title: itemName, image: UIImage(systemName: itemImage)?.withAlignmentRectInsets(.init(top: 10, left: 0, bottom: 0, right: 0)), tag: 0)
         // withAlignmentRectInsets выравниваем (по факту меняем только сверху)
-        item.titlePositionAdjustment = .init(horizontal: 0, vertical: 10)
+        // item.titlePositionAdjustment = .init(horizontal: 0, vertical: 10)
         // т.к. мы сместили картинку, надо будет сместить сам item по вертикали ниже
+        
         // создаём NavController
         let navController = UINavigationController(rootViewController: viewController)
         navController.tabBarItem = item
